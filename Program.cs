9044cad506ec28dc7ba47c7b275eb940d98ae5b0
Александр <129725379+aleks373@users.noreply.github.com>
2023-10-27 15:10:47 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿namespace ConsoleApp1
+{
+
+    //Console.WriteLine("сколько элементов будет в масиве ?");
+    //var count = Int32.Parse(Console.ReadLine());
+    //var array = new int[count];
+    //for (int i = 0; i < count; i++)
+    //{
+    //    array[i] = Int32.Parse(Console.ReadLine());
+    //}
+    //Console.WriteLine("Все элементы записаны ");
+
+
+
+
+
+
+
+
+
+    //Console.WriteLine("как вас зовут");
+    //var name = Console.ReadLine();
+    //var grreting="привет  " + name;
+    //Console.WriteLine(grreting);
+
+
+
+
+
+
+
+
+
+
+    //Console.WriteLine("Сколько элкментов будет в массиве?");
+    //var count = Int32.Parse(Console.ReadLine());
+    //var array = new int[count];
+    //for (int i = 0; i < count; i++)
+    //{
+    //    array[i] = Int32.Parse(Console.ReadLine()); ;
+    //}
+    //Console.WriteLine("все  элементы записанны ");
+
+
+
+
+
+
+
+    //Console.WriteLine( "Введите свой возраст");
+    //var age = Convert.ToInt32( Console.ReadLine() );
+    //if ( age > 13 )
+    //    {
+    //    Console.WriteLine("вы  успешно зарегистривались ");
+    //}
+    //else 
+    //{
+    //    Console.WriteLine("пользователи младше 14 лет не могут быть зарегистрированы");
+    //        }
+}
+
+
+
+
+
---
+++ Task 12.1.5.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<User> users = new List<User>
+        {
+            new User { Login = "user1", Name = "Alice", IsPremium = true },
+            new User { Login = "user2", Name = "Bob", IsPremium = false },
+            new User { Login = "user3", Name = "Charlie", IsPremium = true }
+        };
+
+        foreach (User user in users)
+        {
+            GreetUser(user);
+        }
+    }
+
+    static void GreetUser(User user)
+    {
+        Console.WriteLine("Hello, " + user.Name + "!");
+
+        if (!user.IsPremium)
+        {
+            ShowAds();
+        }
+    }
+
+    static void ShowAds()
+    {
+        Console.WriteLine("Посетите наш новый сайт с бесплатными играми free.games.for.a.fool.com");
+        // Остановка на 1 с
+        Thread.Sleep(1000);
+
+        Console.WriteLine("Купите подписку на МыКомбо и слушайте музыку везде и всегда.");
+        // Остановка на 2 с
+        Thread.Sleep(2000);
+
+        Console.WriteLine("Оформите премиум-подписку на наш сервис, чтобы не видеть рекламу.");
+        // Остановка на 3 с
+        Thread.Sleep(3000);
+    }
+}
+
+class User
+{
+    public string Login { get; set; }
+    public string Name { get; set; }
+    public bool IsPremium { get; set; }
+}
